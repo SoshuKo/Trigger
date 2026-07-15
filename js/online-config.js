88dcc77674cd8e3877ea035dbe002af857386ebc
SoshuKo <chitian.kokkota@gmail.com>
2026-07-15 13:49:52 +0900

    Update online-config.js

--- js/online-config.js
+++ js/online-config.js
@@ -10,8 +10,8 @@
  * enforced by the Row Level Security policies in supabase-schema.sql.
  */
 window.TRION_ONLINE_CONFIG = {
-  enabled: false,
-  supabaseUrl: '',
-  supabaseKey: '',
+  enabled: true,
+  supabaseUrl: 'https://fcjxzhmjyzbanmhmpdgq.supabase.co',
+  supabaseKey: 'sb_publishable_VAmWTiWmnC_0xlOc340i4w_g-K_mXaz',
   snapshotHz: 6,
 };
